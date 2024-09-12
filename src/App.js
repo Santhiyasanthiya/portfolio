e1c59102d5be01e13b6a6b9588bc5d8f11d4cbb7
Santhiya.K <162422504+zuppasanthiya@users.noreply.github.com>
2024-09-12 15:45:14 +0530

    recreate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import { Helmet } from "react-helmet";
 import Navbar from "./components/NavBar/Navbar";
 import "./App.css";
 import Main from "./pages/Main";
@@ -10,6 +10,7 @@ import Contact from "./pages/Contact";
 import { globalStyles } from "./portfolio";
 import Loader from "./components/loaders/Loader";
 import ReactGA from "react-ga";
+
 ReactGA.initialize("UA-208216831-1");
 
 function App() {
@@ -24,6 +25,25 @@ function App() {
 
   const maincontent = (
     <div>
+      <Helmet>
+        <title>Santhiya | Full Stack Developer</title>
+        <meta
+          name="description"
+          content="Hello Everyone, I am Santhiya, a Full Stack Developer working at Zuppa."
+        />
+        <meta
+          name="keywords"
+          content="drones, drone seller, advanced drones, Zuppa, drone technology, drone store, buy drones online"
+        />
+        <meta property="og:title" content="Santhiya | Zuppa Company" />
+        <meta property="og:description" content="Web Developer" />
+        <meta property="og:image" content="link_to_your_image.jpg" />
+        <meta
+          property="og:url"
+          content="https://santhiya-k-portfolio.vercel.app/#/"
+        />
+      </Helmet>
+
       <Navbar />
       <Main />
       <Aboutme />
